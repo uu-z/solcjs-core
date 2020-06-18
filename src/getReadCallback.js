0913af15247a23650a38ad584ae0501dad283f80
uuz <ncysatnaf@gmail.com>
2020-06-18 22:03:16 +0800

    update solc-import

--- src/getReadCallback.js
+++ src/getReadCallback.js
@@ -1,8 +1,8 @@
-const solcImport = require('solc-import');
+const solcImport = require('solc-import-fix')
 
-module.exports = getReadCallback;
+module.exports = getReadCallback
 
 async function getReadCallback(sourcecode, getImportContent) {
-  if (!solcImport.isExistImport(sourcecode)) return;
-  return await solcImport.getReadCallback(sourcecode, getImportContent);
+  if (!solcImport.isExistImport(sourcecode)) return
+  return await solcImport.getReadCallback(sourcecode, getImportContent)
 }
